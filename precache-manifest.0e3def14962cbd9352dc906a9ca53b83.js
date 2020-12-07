af3d18a583d6eb6e0f7c849d89819be56b2b32e9
cortelle <ayomikunalimi@gmail.com>
2020-12-07 22:28:01 +0100

    Updates

---
+++ precache-manifest.0e3def14962cbd9352dc906a9ca53b83.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f511fab0bb30b21677e4359d1107b6",
+    "url": "/React-MovieApp/index.html"
+  },
+  {
+    "revision": "9c68f4ace8a2de52cc9e",
+    "url": "/React-MovieApp/static/js/2.245c07e3.chunk.js"
+  },
+  {
+    "revision": "50ad65d8517bf2f3b40a",
+    "url": "/React-MovieApp/static/js/main.38d55bcd.chunk.js"
+  },
+  {
+    "revision": "32fde22aad509f5aff11",
+    "url": "/React-MovieApp/static/js/runtime-main.3f7721e4.js"
+  }
+]);
